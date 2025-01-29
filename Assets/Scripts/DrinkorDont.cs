5580d6c2498793056d1ecfb8df292a09077f9556
Max <duzypanmajster@gmail.com>
2025-01-29 23:13:03 +0100

    drink or not

---
+++ Assets/Scripts/DrinkorDont.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DrinkorDont : MonoBehaviour
+{
+    public void ScenaPiwko()
+    {
+        //SceneManager.LoadScene("Piwko");
+    }
+
+    public void ScenaWodka()
+    {
+        //SceneManager.LoadScene("Wodka");
+    }
+}
